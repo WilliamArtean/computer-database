42644f717cd44c488b63ccb8ca5137925a06cd35
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-07 16:56:16 +0200

    Created model classes Computer & Company

---
+++ src/model/Company.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Company {
+	
+	private String name;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+}
---
+++ src/model/Computer.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Computer {
+	
+	private String name;
+	private LocalDate introductionDate;
+	private LocalDate discontinuationDate;
+	private Company manufacturer;
+	
+	public Computer(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getIntroductionDate() {
+		return introductionDate;
+	}
+
+	public void setIntroductionDate(LocalDate introductionDate) {
+		this.introductionDate = introductionDate;
+	}
+
+	public LocalDate getDiscontinuationDate() {
+		return discontinuationDate;
+	}
+
+	public void setDiscontinuationDate(LocalDate discontinuationDate) {
+		this.discontinuationDate = discontinuationDate;
+	}
+
+	public Company getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(Company manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+}
