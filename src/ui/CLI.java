5f3b923e909f43f024912667bd5c1ecf866fbe64
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-10 09:55:27 +0200

    Added user input
    The user can input a command. They are not yet recognized, the system throws a IncorrectCommandException every time.

--- src/ui/CLI.java
+++ src/ui/CLI.java
@@ -1,26 +1,48 @@
 package ui;
 
 import exceptions.*;
+import java.io.*;
 
 public class CLI {
 
+	private BufferedReader br;
+	private String input;
+
 	public static void main(String[] args) {
+		CLI interpreter = new CLI();
+		try {
+			interpreter.getInput();
+			interpreter.processInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IncorrectCommandException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public String getInput() {
-		return null;
+	public CLI() {
+		this.br = new BufferedReader(new InputStreamReader(System.in));
 	}
 	
-	public void processInput(String input) {
-		String commandFirstWord = input.substring(0, input.indexOf(" "));
-		try {
-			switch (commandFirstWord) {
-			default:
-				throw new IncorrectCommandException();
-			}
-		} catch (IncorrectCommandException e) {
-			e.printStackTrace();
+	public void getInput() throws IOException {
+		System.out.println("Enter command: ");
+		this.input = this.br.readLine();
+	}
+	
+	/*
+	 * Legal commands :
+	 * 	list computers, list companies
+	 * 	delete computer [computer name]
+	 * 	show computer details [computer name]
+	 */
+	public void processInput() throws IncorrectCommandException {
+		if (this.input.indexOf(' ') == -1) throw new IncorrectCommandException();
+		String commandFirstWord = this.input.substring(0, input.indexOf(' '));
+		
+		switch (commandFirstWord) {
+		default:
+			throw new IncorrectCommandException();
 		}
 		
 	}
