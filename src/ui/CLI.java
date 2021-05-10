3791266f1ac946c106aaeace0b201f075e2980d0
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-10 10:32:57 +0200

    Added list commands processing
    The CLI now recognizes the commands "list computers" and "list companies". An incorrect command throws an IncorrectCommandException. No argument or an argument other than "computers" or "companies" throw an IncorrectArgumentEcxeption.

--- src/ui/CLI.java
+++ src/ui/CLI.java
@@ -17,6 +17,8 @@ public class CLI {
 			e.printStackTrace();
 		} catch (IncorrectCommandException e) {
 			e.printStackTrace();
+		} catch (IncorrectArgumentException e) {
+			e.printStackTrace();
 		}
 		
 	}
@@ -36,13 +38,41 @@ public class CLI {
 	 * 	delete computer [computer name]
 	 * 	show computer details [computer name]
 	 */
-	public void processInput() throws IncorrectCommandException {
-		if (this.input.indexOf(' ') == -1) throw new IncorrectCommandException();
-		String commandFirstWord = this.input.substring(0, input.indexOf(' '));
+	public void processInput() throws IncorrectCommandException, IncorrectArgumentException {
+		String commandFirstWord;
+		if (this.input.indexOf(' ') != -1) {
+			commandFirstWord = this.input.substring(0, this.input.indexOf(' ')).toLowerCase();
+		} else {
+			commandFirstWord = this.input;
+		}
+		 
 		
 		switch (commandFirstWord) {
+		case ("list"):
+			processCommandList();
+			break;
 		default:
 			throw new IncorrectCommandException();
+		}
+		
+	}
+	
+	private void processCommandList() throws IncorrectArgumentException {
+		if (this.input.indexOf(' ') == -1) {
+			throw new IncorrectArgumentException();
+		}
+		
+		String argument1 = this.input.substring(this.input.indexOf(' ')).trim().toLowerCase();
+		
+		switch(argument1) {
+		case ("computers"):
+			System.out.println("List\nof\ncomputers");
+			break;
+		case ("companies"):
+			System.out.println("List\nof\ncompanies");
+			break;
+		default:
+			throw new IncorrectArgumentException();
 		}
 		
 	}
