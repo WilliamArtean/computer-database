36ebf67732726730e4c480e81fe63de6b7f15163
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-11 11:09:21 +0200

    Added ID field and constructor to Company

--- src/model/Company.java
+++ src/model/Company.java
@@ -2,12 +2,21 @@ package model;
 
 public class Company {
 	
+	private long id;
 	private String name;
+	
+	public Company(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 	
 	public Company(String name) {
 		this.name = name;
 	}
 	
+	public long getID() {
+		return this.id;
+	}
 	public String getName() {
 		return this.name;
 	}
