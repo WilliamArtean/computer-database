603daa22fae5de26b86934914ee0d8d8f226ed71
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-17 17:50:29 +0200

    Created ComputerService with get methods

---
+++ src/service/ComputerService.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import model.Computer;
+import persistence.ComputerDAO;
+
+public class ComputerService {
+	
+	private ComputerDAO dao;
+
+	public void setComputerDAO(ComputerDAO computerDAO) {
+		this.dao = computerDAO;
+	}
+	
+	public Optional<Computer> getComputer(String name) {
+		return dao.getByName(name);
+	}
+	
+	public Optional<Computer> getComputer(long id) {
+		return dao.getByID(id);
+	}
+
+	public ArrayList<Computer> getAllComputers() {
+		return dao.getAll();
+	}
+	
+	public ArrayList<Computer> getComputerSelection(int numberToGet, int lineOffset) {
+		return dao.getSelection(numberToGet, lineOffset);
+	}
+}
