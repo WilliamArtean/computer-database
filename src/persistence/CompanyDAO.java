4d6b3a818b2ec89943c7635c7e7decc2ffbacd12
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-17 17:25:16 +0200

    CompanyDAO: SQL Exceptions are now catched inside the class

--- src/persistence/CompanyDAO.java
+++ src/persistence/CompanyDAO.java
@@ -23,55 +23,76 @@ public class CompanyDAO {
 		this.dbManager = databaseManager;
 	}
 	
-	public Optional<Company> getByID(long id) throws SQLException {
-		Connection co = this.dbManager.getNewConnection();
-		PreparedStatement ps = co.prepareStatement(queryGetByID);
-		ps.setLong(1, id);
-		ResultSet rs = ps.executeQuery();
-		Optional<Company> company = mapper.mapToCompany(rs);
-		
-		rs.close();
-		ps.close();
-		co.close();
+	public Optional<Company> getByID(long id) {
+		Optional<Company> company = Optional.empty();
+		try {
+			Connection co = this.dbManager.getNewConnection();
+			PreparedStatement ps = co.prepareStatement(queryGetByID);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			company = mapper.mapToCompany(rs);
+			
+			rs.close();
+			ps.close();
+			co.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return company;
 	}
 	
-	public Optional<Company> getByName(String name) throws SQLException {
-		Connection co = this.dbManager.getNewConnection();
-		PreparedStatement ps = co.prepareStatement(queryGetByName);
-		ps.setString(1, name);
-		ResultSet rs = ps.executeQuery();
-		Optional<Company> company = mapper.mapToCompany(rs);
+	public Optional<Company> getByName(String name) {
+		Optional<Company> company = Optional.empty();
+		try {
+			Connection co = this.dbManager.getNewConnection();
+			PreparedStatement ps = co.prepareStatement(queryGetByName);
+			ps.setString(1, name);
+			ResultSet rs = ps.executeQuery();
+			company = mapper.mapToCompany(rs);
+			
+			rs.close();
+			ps.close();
+			co.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-		rs.close();
-		ps.close();
-		co.close();
 		return company;
 	}
 	
-	public ArrayList<Company> getAll() throws SQLException {
-		Connection co = this.dbManager.getNewConnection();
-		PreparedStatement ps = co.prepareStatement(queryGetAll);
-		ResultSet rs = ps.executeQuery();
-		ArrayList<Company> companies = mapper.mapToCompanyArray(rs);
-		
-		rs.close();
-		ps.close();
-		co.close();
+	public ArrayList<Company> getAll() {
+		ArrayList<Company> companies = new ArrayList<Company>();
+		try {
+			Connection co = this.dbManager.getNewConnection();
+			PreparedStatement ps = co.prepareStatement(queryGetAll);
+			ResultSet rs = ps.executeQuery();
+			companies = mapper.mapToCompanyArray(rs);
+			
+			rs.close();
+			ps.close();
+			co.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return companies;
 	}
 	
-	public ArrayList<Company> getSelection(int numberToReturn, int offset) throws SQLException {
-		Connection co = this.dbManager.getNewConnection();
-		PreparedStatement ps = co.prepareStatement(queryGetSelection);
-		ps.setInt(1, numberToReturn);
-		ps.setInt(2, offset);
-		ResultSet rs = ps.executeQuery();
-		ArrayList<Company> companies = mapper.mapToCompanyArray(rs);
-		
-		rs.close();
-		ps.close();
-		co.close();
+	public ArrayList<Company> getSelection(int numberToReturn, int offset) {
+		ArrayList<Company> companies = new ArrayList<Company>();
+		try {
+			Connection co = this.dbManager.getNewConnection();
+			PreparedStatement ps = co.prepareStatement(queryGetSelection);
+			ps.setInt(1, numberToReturn);
+			ps.setInt(2, offset);
+			ResultSet rs = ps.executeQuery();
+			companies = mapper.mapToCompanyArray(rs);
+			
+			rs.close();
+			ps.close();
+			co.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return companies;
 	}
 	
