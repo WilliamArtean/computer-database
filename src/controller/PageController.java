402fb0030483e366bb6464c57ece70d10d8cf41a
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-18 16:55:20 +0200

    Corrected calculation for number of pages

--- src/controller/PageController.java
+++ src/controller/PageController.java
@@ -34,7 +34,7 @@ public class PageController {
 	public PageController(Page view, ComputerService service) {
 		this.view = view;
 		this.service = service;
-		this.numberOfPages = service.getCount();
+		this.numberOfPages = ((service.getCount() - 1) / itemsPerPage) + 1;
 	}
 	
 	public void startNavigation() throws IOException {
