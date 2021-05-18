87fa1a7a9fe781ade401ee9fb35610d1eab372fe
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-18 15:34:59 +0200

    Created Page class in model package

---
+++ src/model/Page.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Page {
+	
+	private int numberOfPages;
+	private int currentPage;
+	
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+}
