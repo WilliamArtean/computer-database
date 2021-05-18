177ce07a5997f09d02cbde286ab8e090fd2c7d52
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-18 11:26:56 +0200

    Added CLIView class
    Implemented Main Menu Display

---
+++ src/ui/CLIView.java
@@ -0,0 +1,15 @@
+package ui;
+
+public class CLIView {
+	
+	public void displayMainMenu() {
+		System.out.println("1 - Show list of computers");
+		System.out.println("2 - Show list of companies");
+		System.out.println("3 - Show details of a computer");
+		System.out.println("4 - Create a computer");
+		System.out.println("5 - Update a computer");
+		System.out.println("6 - Delete a computer");
+		System.out.println("7 - Exit");
+	}
+
+}
