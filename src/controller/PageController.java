08f3a1f2cb7e4339794e0ebb3e6d27ed389ccc34
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-18 16:14:14 +0200

    Added ComputerService, and list to PageController

--- src/controller/PageController.java
+++ src/controller/PageController.java
@@ -1,15 +1,21 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import model.Computer;
+import service.ComputerService;
 import ui.Page;
 
 public class PageController {
 
 	private Page view;
-	private int currentPage;
+	private ComputerService service;
+	private int currentPage = 0;
 	private int numberOfPages;
-	
+	private final int itemsPerPage = 10;
+	private ArrayList<Computer> list = new ArrayList<Computer>();
+		
 	public int getNumberOfPages() {
 		return numberOfPages;
 	}
@@ -17,8 +23,10 @@ public class PageController {
 		return currentPage;
 	}
 	
-	public PageController(Page view) {
+	public PageController(Page view, ComputerService service) {
 		this.view = view;
+		this.service = service;
+		
 	}
 	
 	public void startNavigation() throws IOException {
