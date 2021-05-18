2c4cb26e4f0f917405395443aecbd1aa692a6b73
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-18 15:48:39 +0200

    Added displayPage method to Page

--- src/ui/Page.java
+++ src/ui/Page.java
@@ -1,9 +1,13 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Page {
 	
 	private int numberOfPages;
 	private int currentPage;
+	private ArrayList<String> list;
 	
 	public int getNumberOfPages() {
 		return numberOfPages;
@@ -12,4 +16,19 @@ public class Page {
 		return currentPage;
 	}
 
+	public void displayPage() {
+		String listToDisplay = list.stream().collect(Collectors.joining("\n"));
+		System.out.println(listToDisplay);
+		
+		StringBuilder pagination = new StringBuilder("\n");
+		pagination.append(currentPage).append("/").append(numberOfPages);
+		if (currentPage < numberOfPages) {
+			pagination.append("\n").append("Press 'n' to move to the next page.");
+		}
+		if (currentPage > 1) {
+			pagination.append("\n").append("Press 'p' to move to the previous page.");
+		}
+		pagination.append("\n").append("Press 'q' to quit page navigation.");
+	}
+	
 }
