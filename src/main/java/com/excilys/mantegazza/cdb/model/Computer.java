10405d82483568272eb99d5be4dc8179e024a241
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-21 09:18:05 +0200

    Overriden equals and hashCode in Computer and Company models

--- src/main/java/com/excilys/mantegazza/cdb/model/Computer.java
+++ src/main/java/com/excilys/mantegazza/cdb/model/Computer.java
@@ -72,7 +72,67 @@ public class Computer {
 		sb.append(']');
 		return sb.toString();
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinuationDate == null) ? 0 : discontinuationDate.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((introductionDate == null) ? 0 : introductionDate.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;			
+		}
+		if (obj == null) {
+			return false;			
+		}
+		if (getClass() != obj.getClass()) {
+			return false;			
+		}
+		Computer other = (Computer) obj;
+		if (company == null) {
+			if (other.company != null) {
+				return false;				
+			}
+		} else if (!company.equals(other.company)) {
+			return false;			
+		}
+		if (discontinuationDate == null) {
+			if (other.discontinuationDate != null) {
+				return false;				
+			}
+		} else if (!discontinuationDate.equals(other.discontinuationDate)) {
+			return false;			
+		}
+		if (id != other.id) {
+			return false;			
+		}
+		if (introductionDate == null) {
+			if (other.introductionDate != null) {
+				return false;				
+			}
+		} else if (!introductionDate.equals(other.introductionDate)) {
+			return false;
+			
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;				
+			}
+		} else if (!name.equals(other.name)) {
+			return false;			
+		}
+		return true;
+	}
+
+
 	
 	
 }
