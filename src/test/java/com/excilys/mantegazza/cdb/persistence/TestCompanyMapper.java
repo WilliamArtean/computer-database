625ca81901d7b7fe07f561f2e6715d1454062059
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-25 16:05:41 +0200

    Test for mapping array of companies

--- src/test/java/com/excilys/mantegazza/cdb/persistence/TestCompanyMapper.java
+++ src/test/java/com/excilys/mantegazza/cdb/persistence/TestCompanyMapper.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
@@ -21,6 +22,7 @@ import com.excilys.mantegazza.cdb.model.Company;
 public class TestCompanyMapper {
 	
 	private final String queryGetByName = "SELECT id, name FROM company WHERE name=?";
+	private final String getLastCompanies = "SELECT id, name FROM company WHERE id>=1000";
 	private final String insertTestCompany = "INSERT INTO company (id, name) VALUES (?,?)";
 	private final String deleteTestCompany = "DELETE FROM company WHERE name=?";
 	
@@ -76,6 +78,63 @@ public class TestCompanyMapper {
 		psGet.close();
 		
 		assertTrue(mappedCompany.isEmpty());
+	}
+	
+	@Test
+	public void mapCompanyArray() throws SQLException {
+		Company company1 = new Company.CompanyBuilder("Test Company 1").withID(1000).build();
+		Company company2 = new Company.CompanyBuilder("Test Company 2").withID(1001).build();
+		Company company3 = new Company.CompanyBuilder("Test Company 3").withID(1002).build();
+		Company company4 = new Company.CompanyBuilder("Test Company 4").withID(1003).build();
+		ArrayList<Company> sourceCompanies = new ArrayList<Company>();
+		sourceCompanies.add(company1);
+		sourceCompanies.add(company2);
+		sourceCompanies.add(company3);
+		sourceCompanies.add(company4);
+		
+		//Inserts the test companies into the in-memory database
+		PreparedStatement psInsert = co.prepareStatement(insertTestCompany);
+		psInsert.setLong(1, company1.getID());
+		psInsert.setString(2, company1.getName());
+		psInsert.addBatch();
+		
+		psInsert.setLong(1, company2.getID());
+		psInsert.setString(2, company2.getName());
+		psInsert.addBatch();
+		
+		psInsert.setLong(1, company3.getID());
+		psInsert.setString(2, company3.getName());
+		psInsert.addBatch();
+		
+		psInsert.setLong(1, company4.getID());
+		psInsert.setString(2, company4.getName());
+		psInsert.addBatch();
+		
+		psInsert.executeBatch();
+		psInsert.close();
+		
+		
+		PreparedStatement psGet = co.prepareStatement(getLastCompanies);
+		ResultSet rs = psGet.executeQuery();
+		ArrayList<Company> mappedCompanies = companyMapperSUT.mapToCompanyArray(rs);
+		assertEquals(sourceCompanies, mappedCompanies);
+		
+		
+		PreparedStatement psDelete = co.prepareStatement(deleteTestCompany);
+		psDelete.setString(1, company1.getName());
+		psDelete.addBatch();
+		
+		psDelete.setString(1, company2.getName());
+		psDelete.addBatch();
+		
+		psDelete.setString(1, company3.getName());
+		psDelete.addBatch();
+		
+		psDelete.setString(1, company4.getName());
+		psDelete.addBatch();
+		
+		psDelete.executeBatch();
+		psDelete.close();
 	}
 	
 }
