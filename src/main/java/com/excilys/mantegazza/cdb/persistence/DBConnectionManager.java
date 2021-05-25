b730a50c6da664e74c2c80f8431534db1b89e5e9
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-25 11:00:31 +0200

    updated DBConnectionManager to fetch database properties from db.properties file

--- src/main/java/com/excilys/mantegazza/cdb/persistence/DBConnectionManager.java
+++ src/main/java/com/excilys/mantegazza/cdb/persistence/DBConnectionManager.java
@@ -1,14 +1,35 @@
 package com.excilys.mantegazza.cdb.persistence;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBConnectionManager {
 	
-	private String url = "jdbc:mysql://localhost:3306/computer-database-db";
-	private String user = "admincdb";
-	private String pswd = "qwerty1234";
+	private String url;
+	private String user;
+	private String password;
+	private Logger logger = LoggerFactory.getLogger(DBConnectionManager.class);
+	
+	public DBConnectionManager() {
+		try {
+			InputStream input = getClass().getClassLoader().getResourceAsStream("db.properties");
+			Properties properties = new Properties();
+			properties.load(input);
+			
+			url = properties.getProperty("url");
+			user = properties.getProperty("user");
+			password = properties.getProperty("password");
+		} catch (IOException e) {
+			logger.error("Unable to load database properties file");
+		}
+	}
 	
 	/**
 	 * Get a new connection to the database. The DBConnectionManager will not close it automatically.
@@ -16,7 +37,7 @@ public class DBConnectionManager {
 	 * @throws SQLException
 	 */
 	public Connection getNewConnection() throws SQLException {
-		return DriverManager.getConnection(url, user, pswd);
+		return DriverManager.getConnection(url, user, password);
 	}
 	
 }
