01cb946c9f4a819e61a4ca9833bc8a61fce5294f
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-26 10:26:36 +0200

    Added last tests for CompanyDAO

--- src/test/java/com/excilys/mantegazza/cdb/persistence/TestCompanyDAO.java
+++ src/test/java/com/excilys/mantegazza/cdb/persistence/TestCompanyDAO.java
@@ -1,12 +1,14 @@
 package com.excilys.mantegazza.cdb.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
@@ -17,12 +19,8 @@ import com.excilys.mantegazza.cdb.model.Company;
 
 public class TestCompanyDAO {
 
-	private final String queryGetByID = "SELECT id, name FROM company WHERE id=?";
-	private final String queryGetByName = "SELECT id, name FROM company WHERE name=?";
 	private final String queryGetAll = "SELECT id, name FROM company";
 	private final String queryGetSelection = "SELECT id, name FROM company ORDER BY id LIMIT ? OFFSET ?";
-	private final String queryGetCount = "SELECT COUNT(id) AS rowcount FROM company";
-	private final String insertTestCompany = "INSERT INTO company (id, name) VALUES (?,?)";
 	
 	private CompanyDAO companyDAOSUT = new CompanyDAO();
 	private DBConnectionManager connectionManager = new DBConnectionManager();
@@ -71,6 +69,49 @@ public class TestCompanyDAO {
 		
 		Optional<Company> fetchedCompany = companyDAOSUT.getByName(sourceCompany.getName());
 		assertTrue(fetchedCompany.isEmpty());
+	}
+	
+	@Test
+	public void getAllCompanies() throws SQLException {		
+		PreparedStatement ps = co.prepareStatement(queryGetAll);
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<Company> sourceCompanies = new ArrayList<Company>();
+		while (rs.next()) {
+			sourceCompanies.add(
+					new Company.CompanyBuilder(rs.getString("name"))
+					.withID(rs.getLong("id"))
+					.build());
+		}
+		
+		ArrayList<Company> fetchedCompanies = companyDAOSUT.getAll();
+		assertFalse(fetchedCompanies.isEmpty());
+		assertEquals(sourceCompanies, fetchedCompanies);
+	}
+	
+	@Test
+	public void getCompanySelection() throws SQLException {
+		PreparedStatement ps = co.prepareStatement(queryGetSelection);
+		ps.setInt(1, 10);
+		ps.setInt(2, 20);
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<Company> sourceCompanies = new ArrayList<Company>();
+		while (rs.next()) {
+			sourceCompanies.add(
+					new Company.CompanyBuilder(rs.getString("name"))
+					.withID(rs.getLong("id"))
+					.build());
+		}
+		
+		ArrayList<Company> fetchedCompanies = companyDAOSUT.getSelection(10, 20);
+		assertFalse(fetchedCompanies.isEmpty());
+		assertEquals(sourceCompanies, fetchedCompanies);
+	}
+	
+	@Test
+	public void getCompanyCount() {
+		assertEquals(42, companyDAOSUT.getCount());
 	}
 	
 }
