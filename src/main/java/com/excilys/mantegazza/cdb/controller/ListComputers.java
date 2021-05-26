ebe819575918866310e57a80b1a245f3c40bac60
Pierre Mantegazza <49361083+WilliamArtean@users.noreply.github.com>
2021-05-26 16:43:59 +0200

    Added call to ListComputerView JSP in matching Servlet

---
+++ src/main/java/com/excilys/mantegazza/cdb/controller/ListComputers.java
@@ -0,0 +1,20 @@
+package com.excilys.mantegazza.cdb.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListComputers extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/ListComputersView.jsp").forward(req, resp);
+	}
+
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/ListComputersView.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Computer List</title>
+    </head>
+
+    <body>
+        <p>List of computers generated through JSP. Voilà, c'est très accentué.</p>
+    </body>
+</html>
